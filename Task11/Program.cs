3e27af040f0f7623d4692d2bdb76ef65d6fc2017
Valeria K <valeriafo@mail.ru>
2022-05-29 22:58:04 +0500

    Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,8 @@
+﻿int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+
+double  firstDigit = num / 100;
+double thirdDigit = num % 10;
+Console.Write(firstDigit);
+Console.Write(thirdDigit);
+

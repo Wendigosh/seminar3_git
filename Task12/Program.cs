0ef824b56e74ef93c23d5bc8817c2b18b2d49298
Valeria K <valeriafo@mail.ru>
2022-05-29 23:19:23 +0500

    digit %

---
+++ Task12/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите первое число:");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(num1 % num2 == 0 ? "Число " + num1 + " кратно числу " + num2 : "Остаток от деления равен " + num1 % num2);

b522f6d196b71efefb483fa9f2ca3ee4efabab89
Valeria K <valeriafo@mail.ru>
2022-05-29 22:43:41 +0500

    new works

---
+++ Task9/Program.cs
@@ -0,0 +1,18 @@
+﻿int num = new Random().Next(10, 100);
+Console.WriteLine(num);
+
+//int firstDigit = num / 10;
+//int secondDigit = num % 10;
+//if (firstDigit > secondDigit) Console.WriteLine($"Max= {firstDigit}");
+//else Console.WriteLine($"Max= {secondDigit}");
+
+//Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit);
+
+int maxNumber = GetMaxDigit(num);
+Console.WriteLine(maxNumber);
+int GetMaxDigit(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
